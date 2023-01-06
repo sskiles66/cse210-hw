c7dc79ab16bea76d981ac61cc95453065385099e
sskiles66 <stephentskiles@hotmail.com>
2023-01-05 19:00:02 -0800

    Completed letter percentage program

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,44 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        Console.Write("What is your grade percentage? ");
+        string percentageString = Console.ReadLine();
+        int percentage = int.Parse(percentageString);
+        Console.WriteLine(percentage);
+        string letter = "";
+        string sign = "";
+        if (percentage >= 90){
+            letter = "A";
+        }
+        else if (percentage >= 80){
+            letter = "B";
+        }
+        else if (percentage >= 70){
+            letter = "C";
+        }
+        else if (percentage >= 60){
+            letter = "D";
+        }else{
+            letter = "F";
+        }
+
+        if (letter == "A" || letter == "B" || letter == "C"){
+            Console.WriteLine("You passed!");
+        }else{
+            Console.WriteLine("Better luck next time.");
+        }
+
+        int find_sign = percentage % 10;
+
+        if (find_sign >= 7 && letter != "A" && letter != "F"){
+            sign = "+";
+        }
+        else if (find_sign < 3 && letter != "F"){
+            sign = "-";
+        }else{
+            sign = "";
+        }
+
+        Console.WriteLine($"{letter}{sign}");
     }
 }
