2604bdec5803b29381c8c39087222c346ddabd79
sskiles66 <stephentskiles@hotmail.com>
2023-01-16 14:06:42 -0700

    Completed Abstraction Learning Activity

--- prepare/Learning02/Program.cs
+++ prepare/Learning02/Program.cs
@@ -4,6 +4,25 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning02 World!");
+        Job job1 = new Job();
+        job1._company = "Apple";
+        job1._jobTitle = "Software Developer";
+        job1._startYear = 2020;
+        job1._endYear = 2022;
+        
+        Job job2 = new Job();
+        job2._company = "Microsoft";
+        job2._jobTitle = "Hardware Consultant";
+        job2._startYear = 2015;
+        job2._endYear = 2017;
+        
+        Resume resume1 = new Resume();
+        resume1._name = "Stephen Skiles";
+        resume1._jobs.Add(job1);
+        resume1._jobs.Add(job2);
+    
+        resume1.Display();
+
+        
     }
 }
