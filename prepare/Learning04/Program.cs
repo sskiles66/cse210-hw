c7c8257a5a14217c45b61706dbf0fd65d9c8991a
sskiles66 <stephentskiles@hotmail.com>
2023-02-20 13:29:33 -0700

    Finished Inheritance Learning Activity

--- prepare/Learning04/Program.cs
+++ prepare/Learning04/Program.cs
@@ -4,6 +4,17 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning04 World!");
+        Assignment assignment1 = new Assignment("Stephen", "Math"); 
+        Console.WriteLine(assignment1.GetSummary());
+
+        Console.WriteLine();
+        MathAssignment assignment2 = new MathAssignment("Stephen", "Math", "7", "11-17"); 
+        Console.WriteLine(assignment2.GetSummary());
+        Console.WriteLine(assignment2.GetHomeworkList());
+
+        Console.WriteLine();
+        WritingAssignment assignment3 = new WritingAssignment("Stephen", "Writing", "Title of some Book");
+        Console.WriteLine(assignment3.GetSummary());
+        Console.WriteLine(assignment3.GetWritingInfo());
     }
 }
